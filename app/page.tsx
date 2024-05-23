17f64f4dbf30fc47ef6a73e6756e35ea1d0066ea
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2024-05-23 11:53:50 -0700

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,18 @@ import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
+// import { FaGithub } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={[
-          {name: 'Home', link: '/', icon: <FaHome />}
-        ]}/>
+          {name: 'Home', link: '/', icon: <FaHome />},
+          // {name: 'GitHub', link: 'https://github.com/ZachCortez', icon: <FaGithub />}
+        ]} />
         <Hero />
         <Grid />
-        
-        
       </div>
     </main>
   );
